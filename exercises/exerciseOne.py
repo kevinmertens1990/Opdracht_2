870d7282ce9b754fef6265a90ca39161c6dd45f2
Manon Kovacs <36693197+ManonK@users.noreply.github.com>
2018-09-14 10:16:57 +0200

    Motivatiebrief Manon

--- exercises/exerciseOne.py
+++ exercises/exerciseOne.py
@@ -2,15 +2,18 @@
 import sys
 
 welcomeMessage = "Welkom bij de Valori FST intake opdracht. Werk zorgvuldig en netjes, succes!"
+motivationLetter = "In de afgelopen maand tijdens de Masterclass is het mij duidelijk geworden dat mijn interesse ligt bij testautomatisering en programmeren. Gecombineerd met mijn communicatie skills en achtergrond in sales en marketing denk ik dat dit een goede match zou zijn."
 
 def printWelcomeText():
     welcome = welcomeMessage
     return welcome
 
 def printExercise():
-    return None
+    exercise = motivationLetter
+    return exercise
 
 if len(welcomeMessage) != 76:
     sys.exit("Nice try, but not quite right :)")
 
 print(printWelcomeText())
+print(printExercise())
