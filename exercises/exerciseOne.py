72e7208bc70984fdda7b5c5c2d63adefc695ce79
Aka-Fibonacci <43265030+Aka-Fibonacci@users.noreply.github.com>
2018-09-14 10:22:55 +0200

    Add files via upload

--- exercises/exerciseOne.py
+++ exercises/exerciseOne.py
@@ -8,9 +8,12 @@ def printWelcomeText():
     return welcome
 
 def printExercise():
+    ("VFST is niet iets waar mijn hart ligt. Ik ga op zoek naar een andere chapter, Het ligt niet aan jou maar aan mij. Sorry dat ik via text met je verbreek en niet persoonlijk.")
     return None
 
 if len(welcomeMessage) != 76:
     sys.exit("Nice try, but not quite right :)")
 
 print(printWelcomeText())
+
+print(printExercise())
