592ab513e5cdf2199a958f281d144eeb3bdf0db5
Aka-Fibonacci <43265030+Aka-Fibonacci@users.noreply.github.com>
2018-09-14 10:30:48 +0200

    Update exerciseOne.py

--- exercises/exerciseOne.py
+++ exercises/exerciseOne.py
@@ -8,11 +8,14 @@ def printWelcomeText():
     return welcome
 
 def printExercise():
-    ("VFST is niet iets waar mijn hart ligt. Ik ga op zoek naar een andere chapter, Het ligt niet aan jou maar aan mij. Sorry dat ik via text met je verbreek en niet persoonlijk.")
-    return None
+    motivation = Exercise
+    return motivation
 
 if len(welcomeMessage) != 76:
     sys.exit("Nice try, but not quite right :)")
+
+if len(Exercise) != 76:
+    sys.exit ("VFST is niet iets waar mijn hart ligt. Ik ga op zoek naar een andere chapter, Het ligt niet aan jou maar aan mij. Sorry dat ik via text met je verbreek en niet persoonlijk.")
 
 print(printWelcomeText())
 
